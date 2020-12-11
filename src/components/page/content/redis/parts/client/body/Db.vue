5f4d2bf759d209b0b395347bcdebc546f486ec5c
xiabo <bo.xia@joojtech.com>
2020-12-11 13:23:15 +0800

    db body界面

--- src/components/page/content/redis/parts/client/body/Db.vue
+++ src/components/page/content/redis/parts/client/body/Db.vue
@@ -2,33 +2,53 @@
     <div class="db">
         <!--<div class="shell"></div>-->
         <div class="left">
-            <div class="keys">
-                <ol class="keys_ol">
-                    <li>ddddfffffffffffffffffffffdd</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                    <li>aaaaaaa</li>
-                </ol>
+            <div class="list">
+                <div class="keys">
+                    <ol class="keys_ol">
+                        <li>ddddfffffffffffffffffffffdd</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                        <li>aaaaaaa</li>
+                    </ol>
+                </div>
+                <div class="btn"><input type="button" value="Add" /></div>
             </div>
-            <div class="btn"><input type="button" value="Add" /></div>
         </div>
-        <div class="right"></div>
+        <div class="right">
+            <div class="top">
+                <div class="attr">
+                    <div class="type"><span>string</span></div>
+                    <div class="ttl"><span>ttl:99</span></div>
+                </div>
+                <div class="op">
+                    <div class="vas"><select>
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                        </select></div>
+                    <div class="reload"><button>reload</button></div>
+                    <div class="copy"><button>copy</button></div>
+                    <div class="del"><button>delete</button></div>
+                    <div class="save"><button>save</button></div>
+                </div>
+            </div>
+            <div class="view"></div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+
+    }
 </script>
 
 <style lang="css" src="./db.css" scoped>
