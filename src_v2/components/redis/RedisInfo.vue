fe2ee9e7454e6d7815c210723156656485e42e88
xiabo <bo.xia@joojtech.com>
2020-12-29 13:19:16 +0800

    界面修改

--- src_v2/components/redis/RedisInfo.vue
+++ src_v2/components/redis/RedisInfo.vue
@@ -1,19 +1,46 @@
 <template>
     <div class="info">
         <div class="body">
-            <div class="db"></div>
-            <div class="parts"></div>
-            <div class="params"></div>
+            <div class="db">
+                <div class="header">
+                    <span>DB</span><span>Keys</span><span>Expires</span><span>Avg TTL</span><span></span>
+                </div>
+                <div class="list">
+                    <div class="row">
+                        <span>DB</span><span>Keys</span><span>Expires</span><span>Avg TTL</span><div class="ibutton"><span><IButton val="open" /></span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="parts">
+                <div class="part">
+                    <div class="title">cpu</div>
+                    <div class="params">
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                        <div class="param"><span>aaa</span><span>nnn</span></div>
+                    </div>
+                </div>
+                <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+                </div>
         </div>
     </div>
-</template>l
+</template>
 
 <script>
+import IButton from 'components/common/IButton.vue';
+
 export default {
-    
+    components:{
+        IButton
+    }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style src="style/redis/redis-info.css" scoped></style>
