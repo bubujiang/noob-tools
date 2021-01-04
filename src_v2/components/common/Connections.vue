8a08037f6780cf62453d55c6a7f43718b078e8af
x <x>
2021-01-04 08:06:00 +0800

    新服务器连接

---
+++ src_v2/components/common/Connections.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="connections">
+    <div class="content">
+      <div class="button">
+        <IButton val="add_connection" v-on:b-click="()=>{}" />
+      </div>
+      <div class="list" v-if="Object.keys(connections).length">
+        <div class="item" v-for="(connection,i) in connections" v-bind:key="i">
+          <span>{{connection.name}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IButton from "components/common/IButton.vue";
+
+export default {
+    components: {
+      IButton,
+    },
+    computed: {
+      ...mapState("AStore", ["connections"]),
+    },
+}
+</script>
+
+<style src="style/common/connections.css" scoped></style>
--- src_v2/store/app.store.js
+++ src_v2/store/app.store.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
+import Vue from "vue";
 const state = {
-  navigation: [{
+  navigation: [
+    {
       name: "redis",
       page: "Redis",
     },
@@ -11,15 +12,53 @@ const state = {
   ],
   show_navigation: true,
   current_page: "Redis",
-  prompts: [/*{
+  prompts: [
+    /*{
     showed: false, //false没显示过,true显示过
     type: 'error', //error,notice,success
     level: '0', //0右下角提示,1附加弹窗
     info: 'test1',
     time: 123123324
-  }*/],
+  }*/
+  ],
+  connections: [
+    {
+      type: "folder",
+      name: "fa",
+      child: [],
+    },
+    {
+      type: "redis",
+      name: "mine",
+      host: "182.61.12.213",
+      port: 6379,
+      auth: "wGkfv`~@r&bv*7^%",
+      state: 0, //-1连接出错，0未连接，1,正在连接, 2已连接
+    },
+    {
+      type: "folder",
+      name: "fb",
+      child: [
+        {
+          type: "folder",
+          name: "fba",
+          child: [
+            {
+              type: "mysql",
+              name: "mine2",
+              host: "182.61.12.213",
+              port: 6379,
+              user: "wGkfv`~@r&bv*7^%",
+              pwd: "sdddd",
+              state: 0, //-1连接出错，0未连接，1,正在连接, 2已连接
+            },
+          ],
+        },
+      ],
+    },
+  ],
   //show_prompt: false
-}
+};
 
 const mutations = {
   changePage(state, page) {
@@ -28,49 +67,42 @@ const mutations = {
   toggleShowNavigation(state) {
     state.show_navigation = !state.show_navigation;
   },
-  addPromp(state, {
-    type,
-    level,
-    info
-  }) {
+  addPromp(state, { type, level, info }) {
     Vue.set(state.prompts, state.prompts.length, {
-      showed:0,
+      showed: 0,
       type,
       level,
       info,
-      time: new Date().getTime()
-    })
+      time: new Date().getTime(),
+    });
   },
   //showPrompt(state) {
   //  state.show_prompt = true
   //},
-  showedPrompt(state,i) {
-    console.log('start',i);
+  showedPrompt(state, i) {
+    //console.log('start',i);
     Vue.set(state.prompts, i, {
-      ...state.prompts[i],showed:true
-    })
-    console.log('end',state.prompts);
-  }
-}
+      ...state.prompts[i],
+      showed: true,
+    });
+    //console.log('end',state.prompts);
+  },
+};
 
 const actions = {
-  addNewPromp(context, {
-    type,
-    level,
-    info
-  }) {
-    context.commit('addPromp', {
+  addNewPromp(context, { type, level, info }) {
+    context.commit("addPromp", {
       type,
       level,
-      info
-    })
+      info,
+    });
     //context.commit('showPrompt')
-  }
-}
+  },
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions
+  actions,
 };
