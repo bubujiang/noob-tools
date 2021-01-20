4632f6405e3eb7f2cdb8dba7dd2c973484601b56
xiabo <bo.xia@joojtech.com>
2021-01-20 18:18:40 +0800

    添加文件夹

---
+++ src_v2/components/common/AddConnectionFolder.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="widow">
+        <div class="content">
+            <div class="name"><span>{{$t('folder')}}:</span></div>
+            <div class="input"><span><IInput v-model="folder" /></span></div>
+        </div>
+        <div class="op">
+            <div class="cancel"><IButton val="cancel" v-on:b-click="$emit('close')" color="#5d6e93" /></div>
+            <div class="submit"><IButton val="submit" v-on:b-click="$emit('create',folder)" color="#031234" /></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import IInput from "components/common/IInput.vue";
+import IButton from "components/common/IButton.vue";
+
+export default {
+    components:{
+        IInput,
+        IButton
+    },
+    data(){
+        return {
+            folder:''
+        }
+    }
+}
+</script>
+
+<style src="style/common/add-connection-folder.css" scoped></style>
--- src_v2/components/common/Connections.vue
+++ src_v2/components/common/Connections.vue
@@ -7,7 +7,7 @@
       <div class="new" v-show="show" v-on:mouseleave="moveOut" ref="new">
         <div style="height:37px;"></div>
         <div class="new-type">
-          <div class="new-folder">
+          <div class="new-folder" v-on:click="folder_show=true">
             <img :src="require('image/new_folder.png')" />
           </div>
           <div class="new-connect" v-on:click="addConnection">
@@ -21,12 +21,15 @@
       </div>
     </div>
     <span class="handle" @mousedown="mouseDown"><img :src="require('image/drag.png')" /></span>
+    <AddConnectionFolder v-if="folder_show" v-on:close="folder_show = false" v-on:create="createFolder($event)" />
   </div>
 </template>
 
 <script>
   import IButton from "components/common/IButton.vue";
   import Connection from "components/common/Connection.vue";
+  import AddConnectionFolder from "components/common/AddConnectionFolder.vue";
+
   import {
     mapMutations
   } from "vuex";
@@ -38,7 +41,7 @@
   export default {
     components: {
       IButton,
-      Connection
+      Connection,AddConnectionFolder
     },
     props: {
       module: {
@@ -51,7 +54,8 @@
         pre_x: 0,
         pre_w: 150,
         width: 150,
-        show: false
+        show: false, //创建类型列表显示与否
+        folder_show: false //创建文件夹弹窗显示与否
       };
     },
     computed: {
@@ -93,6 +97,9 @@
       },
       moveOut() {
         this.show = false;
+      },
+      createFolder(folder){
+        console.log('create folder',folder);
       }
     }
   }
